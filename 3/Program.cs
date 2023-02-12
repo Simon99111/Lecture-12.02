e655981fc818ea97386daf73262f7e0d4c7cf2da
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-12 13:51:30 +0300

    проверка

---
+++ 3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 2, 33, 8, 54, 6, 7, 8 };
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ 4/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection) //присвоил рандомные значения массиву
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col) //распечатал все элементы
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Index0f(array, 444);
+Console.WriteLine(pos);
